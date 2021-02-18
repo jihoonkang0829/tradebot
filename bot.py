0745f7e20f54881a086ab874c320e547716319e5
Jihoon Kang <jihoon.kang@sibelhealth.com>
2021-02-18 02:49:18 -0600

    Structure update

---
+++ bot.py
@@ -0,0 +1,29 @@
+from assets.algo import algo
+from binance.client import Client
+class Bot:
+    def __init__(self, key, secret_key): 
+        self.client = Client(api_key=key, secret_key=secret_key)
+        self.balance
+        self.current_data
+        self
+
+    def feed_data(self):
+        # client get current data
+        # client get current position
+        # client get current balance 
+        while(True):
+            try:
+                # client get current data
+                # client get current position
+                # client get current balance 
+                break
+            except:
+                pass
+
+        # return algo(current_data, balance)
+
+    # def receive_decision(self):
+    #     return self.decision
+
+
+    
---
+++ main.py
@@ -0,0 +1,18 @@
+from bot import Bot
+
+
+
+# client
+# trader = new bot()
+# while (True):
+#     client.receive_realtime_price
+#     trader.feed_data
+#     trader.receive_decision
+#     if buy:
+#         client.buy
+#     elif sell:
+#         client.sell
+
+# trader = bot(key, secret_key)
+while True():
+    trader.feed_data()
